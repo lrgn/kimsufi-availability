897cc246a4e276dae470c5c502042474d62995bf
Kebigon <git@kebigon.xyz>
2018-07-09 22:17:05 +0900

    Replace e-mail notification by Telegram

--- kimsufi.py
+++ kimsufi.py
@@ -2,72 +2,25 @@
 # -*- coding: utf-8 -*-
 
 """
-Find the Kimsufi servers availability.
-
-Usage:
-  kimsufi.py [options]
-  kimsufi.py <model>... [options]
-
-Options:
-  -h, --help     Show this help.
-  -v, --version  Show version.
-  -m, --mail     Sends a mail when a server is available.
-
-Examples:
-  kimsufi.py
-  kimsufi.py KS-1 KS-3
-  kimsufi.py KS-1 --mail
+Find the Kimsufi servers availability, and send a message via Telegram when a server is available.
 """
 
 import sys
-import smtplib
 
 import requests
 import json
 import os
 
-from docopt import docopt
+from telegram.ext import Updater, CommandHandler
+
+import config
 
 VERSION = "1.0"
 
 API_URL = "https://ws.ovh.com/dedicated/r2/ws.dispatcher/getAvailability2"
 REFERENCES = {
-    "150sk10": "KS-1",
-    "150sk20": "KS-2",
-    "150sk21": "KS-2",
-    "150sk22": "KS-2 SSD",
-    "150sk30": "KS-3",
-    "150sk31": "KS-3",
-    "150sk40": "KS-4",
-    "150sk41": "KS-4",
-    "150sk42": "KS-4",
-    "150sk50": "KS-5",
-    "150sk60": "KS-6",
-
-    "141game1": "GAME-1",
-    "141game2": "GAME-2",
-    "141game3": "GAME-3",
-
-    "142sys4": "SYS-IP-1",
-    "142sys5": "SYS-IP-2",
-    "142sys8": "SYS-IP-4",
-    "142sys6": "SYS-IP-5",
-    "142sys10": "SYS-IP-5S",
-    "142sys7": "SYS-IP-6",
-    "142sys9": "SYS-IP-6S",
-
-    "143sys13": "E3-SSD-1",
-    "143sys10": "E3-SSD-2",
-    "143sys11": "E3-SSD-3",
-    "143sys12": "E3-SSD-4",
-
-    "143sys4": "E3-SAT-1",
-    "143sys1": "E3-SAT-2",
-    "143sys2": "E3-SAT-3",
-    "143sys3": "E3-SAT-4",
-
-    "141bk1": "BK-8T",
-    "141bk2": "BK-24T"
+    "1801sk12": "KS-1 France",
+    "1804sk12": "KS-1 Canada"
 }
 
 ZONES = {'gra': 'Gravelines',
@@ -87,15 +40,15 @@ def get_zone_name(zone):
         return zone
 
 
-def get_servers(models):
+def get_servers():
     """Get the servers from the OVH API."""
+
+    print("Get servers...")
 
     r = requests.get(API_URL)
     response = r.json()['answer']['availability']
 
     search = REFERENCES
-    if models:
-        search = {k: v for k, v in REFERENCES.items() if v in models}
 
     return [k for k in response if any(r == k['reference'] for r in search)]
 
@@ -106,62 +59,8 @@ def get_ref(name):
     return list(REFERENCES.keys())[list(REFERENCES.values()).index(name)]
 
 
-def send_mail(output, total):
-    try:
-        with open(os.path.join(CURRENT_PATH, 'config.json')) as data:
-            config = json.load(data)
-            mail_host = config['email']['host']
-            mail_port = config['email']['port']
-            mail_username = config['email']['username']
-            mail_password = config['email']['password']
-            mail_from = config['email']['mail_from']
-            mail_to = config['email']['mail_to']
-
-    except IOError:
-        print('Rename config.json.sample to config.json and edit it')
-        return False
-
-    """Send a mail to <mail_to>."""
-
-    subject = "{0} server{1} {2} available on Kimsufi".format(
-        total,
-        "s"[total <= 1:],
-        ["is", "are"][total > 1]
-    )
-    headers = "From: {}\r\nTo: {}\r\nSubject: {}\r\n\r\n".format(
-        mail_from,
-        mail_to,
-        subject
-    )
-
-    try:
-        server = smtplib.SMTP(mail_host, mail_port)
-    except smtplib.socket.gaierror:
-        return False
-
-    server.ehlo()
-    server.starttls()
-    server.ehlo()
-
-    try:
-        server.login(mail_username, mail_password)
-    except smtplib.SMTPAuthenticationError:
-        print('SMPT Auth Error!')
-        return False
-
-    try:
-        server.sendmail(mail_from, mail_to, headers + output)
-        return True
-    except Exception:
-        print('Error sending email!')
-        return False
-    finally:
-        server.close()
-
-
-if __name__ == '__main__':
-    arguments = docopt(__doc__, version=VERSION)
-    kim = get_servers(arguments['<model>'])
+def sayhi(bot, job):
+    kim = get_servers()
 
     total = 0
     output = ""
@@ -184,8 +83,18 @@ if __name__ == '__main__':
     )
 
     if total != 0:
-        if arguments['--mail']:
-            print(output)
-            send_mail(output, total)
-        else:
-            print(output)
+        job.context.message.reply_text(output)
+
+
+def start(bot, update, job_queue):
+    bot.sendMessage(chat_id=update.message.chat_id, text="Hello there! I'll keep an eye on Kimsufi, and send you a message when a server is available.")
+    job = job_queue.run_repeating(sayhi, 300, context=update)
+
+
+if __name__ == '__main__':
+    updater = Updater(token=config.API_KEY)
+    dispatcher = updater.dispatcher
+    start_handler = CommandHandler('start', start, pass_job_queue=True)
+    dispatcher.add_handler(start_handler)
+    updater.start_polling()
+    updater.idle()
